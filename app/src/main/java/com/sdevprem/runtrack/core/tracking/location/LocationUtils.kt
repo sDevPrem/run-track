3119a7f1b07cff59fa739dc7a791c9991ad0f223
Prem Thakur <sdevpremthakur@gmail.com>
2023-08-21 13:28:35 +0530

    Check and request enable gps setting

---
+++ app/src/main/java/com/sdevprem/runtrack/core/tracking/location/LocationUtils.kt
@@ -0,0 +1,40 @@
+package com.sdevprem.runtrack.core.tracking.location
+
+import android.app.Activity
+import android.content.IntentSender
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.Priority
+import com.google.android.gms.location.SettingsClient
+
+object LocationUtils {
+    private const val LOCATION_UPDATE_INTERVAL = 5000L
+
+    val locationRequestBuilder
+        get() = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            LOCATION_UPDATE_INTERVAL
+        )
+
+    fun checkAndRequestLocationSetting(activity: Activity) {
+        val locationRequest = locationRequestBuilder.build()
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(activity)
+
+        client.checkLocationSettings(builder.build())
+            .addOnFailureListener { exception ->
+                if (exception is ResolvableApiException) {
+                    try {
+                        exception.startResolutionForResult(
+                            activity,
+                            locationRequest.priority
+                        )
+                    } catch (_: IntentSender.SendIntentException) {
+                    }
+                }
+            }
+    }
+}
--- app/src/main/java/com/sdevprem/runtrack/di/AppModule.kt
+++ app/src/main/java/com/sdevprem/runtrack/di/AppModule.kt
@@ -8,11 +8,13 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.room.Room
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.sdevprem.runtrack.core.data.db.RunTrackDB
 import com.sdevprem.runtrack.core.data.db.RunTrackDB.Companion.RUN_TRACK_DB_NAME
 import com.sdevprem.runtrack.core.tracking.location.DefaultLocationTrackingManager
 import com.sdevprem.runtrack.core.tracking.location.LocationTrackingManager
+import com.sdevprem.runtrack.core.tracking.location.LocationUtils
 import com.sdevprem.runtrack.core.tracking.notification.DefaultNotificationHelper
 import com.sdevprem.runtrack.core.tracking.notification.NotificationHelper
 import com.sdevprem.runtrack.core.tracking.service.DefaultTrackingServiceManager
@@ -72,13 +74,21 @@ abstract class AppModule {
                 produceFile = { context.preferencesDataStoreFile(USER_PREFERENCES_FILE_NAME) },
                 scope = scope.plus(ioDispatcher + SupervisorJob())
             )
-    }
 
-    @Binds
-    @Singleton
-    abstract fun provideLocationTrackingManager(
-        defaultLocationTrackingManager: DefaultLocationTrackingManager
-    ): LocationTrackingManager
+        @Singleton
+        @Provides
+        fun provideLocationTrackingManager(
+            @ApplicationContext context: Context,
+            fusedLocationProviderClient: FusedLocationProviderClient,
+        ): LocationTrackingManager {
+            return DefaultLocationTrackingManager(
+                fusedLocationProviderClient = fusedLocationProviderClient,
+                context = context,
+                locationRequest = LocationUtils.locationRequestBuilder.build()
+            )
+        }
+
+    }
 
     @Binds
     @Singleton
