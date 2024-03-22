7eb7214e52cf219aae5e9cd694f56b1b8b405f0b
Prem Thakur <sdevpremthakur@gmail.com>
2024-03-22 20:03:43 +0530

    feat: create run stats ViewModel

--- app/src/main/java/com/sdevprem/runtrack/utils/DateUtils.kt
+++ app/src/main/java/com/sdevprem/runtrack/utils/DateUtils.kt
@@ -1,6 +1,7 @@
 package com.sdevprem.runtrack.utils
 
 import java.util.Calendar
+import java.util.Date
 
 fun Calendar.setDateToWeekFirstDay() = apply {
     val firstDay = getActualMinimum(Calendar.DAY_OF_WEEK)
@@ -26,4 +27,27 @@ fun Calendar.setMaximumTime() = apply {
     set(Calendar.MINUTE, getActualMaximum(Calendar.MINUTE))
     set(Calendar.SECOND, getActualMaximum(Calendar.SECOND))
     set(Calendar.MILLISECOND, getActualMaximum(Calendar.MILLISECOND))
+}
+
+fun Date.toCalendar(): Calendar = Calendar.getInstance().also { it.time = this }
+
+operator fun ClosedRange<Calendar>.iterator() = object : Iterator<Calendar> {
+    var current = start
+
+    override fun hasNext(): Boolean {
+        (1..2).toList()
+        return current <= endInclusive
+    }
+
+    override fun next(): Calendar {
+        val temp = current
+        current = (current.clone() as Calendar).apply {
+            add(Calendar.DAY_OF_WEEK, 1)
+        }
+        return temp
+    }
+}
+
+fun ClosedRange<Calendar>.toList(): List<Calendar> = buildList {
+    for (c in this@toList) add(c)
 }
