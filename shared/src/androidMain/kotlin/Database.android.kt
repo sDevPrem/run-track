753a220ce58022ba81a79ce340ebc8f5dd7ce689
Prem Thakur <sdevpremthakur@gmail.com>
2026-01-16 21:09:35 +0530

    fix: db name

--- shared/src/androidMain/kotlin/Database.android.kt
+++ shared/src/androidMain/kotlin/Database.android.kt
@@ -2,10 +2,11 @@ import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.sdevprem.runtrack.shared.data.db.RunTrackDB
+import com.sdevprem.runtrack.shared.data.db.RunTrackDB.Companion.RUN_TRACK_DB_NAME
 
 fun getDatabaseBuilder(context: Context): RoomDatabase.Builder<RunTrackDB> {
     val appContext = context.applicationContext
-    val dbFile = appContext.getDatabasePath("my_room.db")
+    val dbFile = appContext.getDatabasePath(RUN_TRACK_DB_NAME)
     return Room.databaseBuilder<RunTrackDB>(
         context = appContext,
         name = dbFile.absolutePath
