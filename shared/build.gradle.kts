1f48805a76979b4bb4d4374dce30c16360200584
Prem Thakur <sdevpremthakur@gmail.com>
2026-01-31 22:48:00 +0530

    Revert "feat: add google map dependency for ios"
    This reverts commit 8461740053ddf8b2d8d71afe5cdecf08bfb41c32.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -31,17 +31,6 @@ kotlin {
             baseName = "shared"
             isStatic = false
         }
-
-        pod("GoogleMaps") {
-            version = libs.versions.pods.google.maps.get()
-            extraOpts += listOf("-compiler-option", "-fmodules")
-
-            pod("Google-Maps-iOS-Utils") {
-                moduleName = "GoogleMapsUtils"
-                version = libs.versions.pods.google.ios.maps.utils.get()
-                extraOpts = listOf("-compiler-option", "-fmodules")
-            }
-        }
     }
 
     sourceSets {
