d9fa5b9861d581aeeba9adf341eb703ae46145d9
Prem Thakur <sdevpremthakur@gmail.com>
2026-02-03 00:22:26 +0530

    feat: create DefaultTrackingManager.ios.kt

--- shared/src/iosMain/kotlin/com/sdevprem/runtrack/shared/di/KoinInitIos.kt
+++ shared/src/iosMain/kotlin/com/sdevprem/runtrack/shared/di/KoinInitIos.kt
@@ -3,6 +3,7 @@ package com.sdevprem.runtrack.shared.di
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import com.sdevprem.runtrack.shared.createDataStore
+import com.sdevprem.runtrack.shared.data.tracking.location.DefaultLocationTrackingManager
 import getDatabaseBuilder
 import getRoomDatabase
 import org.koin.core.context.startKoin
@@ -16,6 +17,7 @@ fun initKoinIos() {
             module {
                 single<DataStore<Preferences>> { createDataStore() }
                 single { getRoomDatabase(getDatabaseBuilder()) }
+                single { DefaultLocationTrackingManager() }
             },
             AppModule().module,
             )
